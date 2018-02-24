606cfb7f3ec76797ba3703ad1ee8a1db3ae8155e
Emme Porter DeWitt <empdewitt@gmail.com>
2018-02-24 07:03:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,5 @@
 def valid_move?(board, index)
-  if index.between?(1,9)
-    if !position_taken?(board, index)
+  if index.between?(1,9) || !position_taken?(board, index)
       true
     else
       false
