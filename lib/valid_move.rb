903d3e9eec766e44ae4630296c6117b450b8a707
Emme Porter DeWitt <empdewitt@gmail.com>
2018-02-24 07:14:54 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,7 @@
 def valid_move?(board, index)
   if board[index] == " " || board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     true
+  if board[index] == "X" || board[index] == "O" ||board[index] == nil
+    false
   end
 end
